a716d83f5721d3d2361768b9c18d6e3d7efaefad
Harry Hedger <hedgerh@gmail.com>
2016-08-30 22:34:58 -0400

    Moved index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import P from './src/plugins/Provider'
-import W from './src/plugins/Webcam'
-
-export const Provider = P
-export const Webcam = W
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import P from './plugins/Provider'
+import W from './plugins/Webcam'
+
+export const Provider = P
+export const Webcam = W

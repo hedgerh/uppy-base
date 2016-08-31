0b9a8a1395e5a0a4d96a1fc2011813b87011fe56
Harry Hedger <hedgerh@gmail.com>
2016-08-31 15:01:45 -0400

    Added note about importing in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// This was the only way I could figure out how to get modules to import correctly.
+// There's definitely a cleaner way to do this!
 import D from './plugins/DragDrop'
 import M from './plugins/Multipart'
 import P from './plugins/Provider'

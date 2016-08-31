81d73ed5542762774acb69e003b6c1bede27f604
Harry Hedger <hedgerh@gmail.com>
2016-08-31 15:01:14 -0400

    Updated index.js to include DragDrop, Multipart, Tus10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import D from './plugins/DragDrop'
+import M from './plugins/Multipart'
 import P from './plugins/Provider'
+import T from './plugins/Tus10'
 import W from './plugins/Webcam'
 
+export const DragDrop = D
+export const Multipart = M
 export const Provider = P
+export const Tus10 = T
 export const Webcam = W
